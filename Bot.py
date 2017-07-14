dc425e9bf5d5bc9a91e0d18629109c0bbc620e9c
NickdogeDev <nickdogegaming@yahoo.com>
2017-07-14 15:47:46 -0400

    more work

--- Bot.py
+++ Bot.py
@@ -28,22 +28,27 @@ async def on_ready():
 	
 @client.event
 async def on_message(message):
-    if message.content == "fuck you"  and not message.author == client.user:
+    # This is a bit messy, should be cleaned up and reorganized soon.
+    msgContent = message.content.lower()
+    if "fuck you" in msgContent and not message.author == client.user:
     	await client.send_message(message.channel, "fuck YOU too %s. I hope you fucking die. Fucker." % message.author)
 		
-    elif message.content == "kill yourself" and not message.author == client.user:
+    elif "kill myself" in msgContent or "kms" in msgContent and not message.author == client.user:
     	await client.send_message(message.channel, "Yeah, you should do it. Please.")   
 		
-    elif message.content == "What game am i playing?" or message.content == "what game am i playing?" or message.content == "what game am i playing" or message.content == "What game am i playing" and not message.author == client.user:
+    elif "what game am i playing" in msgContent and message.author == client.user:
     	await client.send_message(message.channel, "%s you dumbfuck." % message.author.game)
 		
-    elif message.content == "roast me" and not message.author == client.user:
-    	await client.send_message(message.channel, random.choice(lines))  
+    elif "roast me" in msgContent and not message.author == client.user:
+    	await client.send_message(message.channel, random.choice(lines))
 		
+    elif "am i gay" in msgContent and not message.author == client.user:
+    	await client.send_message(message.channel, random.choice(['yes', 'no']))
+		
+	# TODO: Make a list of commands so if we need to do ~help it will make it easier for us...
     elif message.content.startswith(MSG_PREFIX) and not message.author == client.user:
         if message.content == ("{0}ping".format(MSG_PREFIX)) and not message.author == client.user:
-    	    await client.send_message(message.channel, "nigga, what u want?")   
-		
+    	    await client.send_message(message.channel, "nigga, what u want?")   		
     	
 print("Running Bot...")
 client.run(jdata["TOKEN"])
